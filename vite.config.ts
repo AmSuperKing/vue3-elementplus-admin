0d37a0c666f90b03e240353ff613e1ba6be08f7f
wangjian <wangjian5092@163.com>
2026-06-04 18:22:17 +0800

    fix:preview unable problem

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ command, mode }) => {
   console.log('server start mode: =>', mode)
 
   return {
-    base: './',
+    base: '/vue3-elementplus-admin/',
     plugins: [
       vue(),
       vueJsx(),
