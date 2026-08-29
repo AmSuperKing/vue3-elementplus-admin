808bb28791d0b7166754d87c794a84f829ac4a04
wangjian <wangjian5092@163.com>
2026-08-29 17:06:23 +0800

    config: update linter config

--- eslint.config.ts
+++ eslint.config.ts
@@ -16,7 +16,7 @@ export default defineConfigWithVueTs(
     files: ['**/*.{vue,ts,mts,tsx}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**', "**/public/**"]),
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/node_modules/**', "**/public/**", "**/mock/**", "**/logs/**", "**/temp/**", "**/scripts/**"]),
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
